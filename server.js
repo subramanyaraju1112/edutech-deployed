1b2b0de2facf46d0d8bc0effa8c5562656aa2904
Vinayaka Raju <vinayakaraju@VINAY-MBP.local>
2022-12-25 14:54:52 +0530

    port change

--- server.js
+++ server.js
@@ -8,6 +8,6 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(8000, () => {
+app.listen(process.env.PORT, () => {
     console.log("Listening to PORT 8000")
 });
